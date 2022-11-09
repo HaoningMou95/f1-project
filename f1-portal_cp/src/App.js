1c52c370bbc88a633e8752eb7e1eb4589adf1796
HaoningMou <muhaoning@hotmail.com>
2022-11-09 15:16:01 +1100

    recover

---
+++ f1-portal_cp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './components/NavBar'
+import MainBody from './components/MainBody'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+      <MainBody />
+    </div>
+  )
+}
+
+export default App
---
+++ f1-portal_cp/src/components/MainBody.js
@@ -0,0 +1,25 @@
+import '../styles/MainBody.css'
+
+export default function MainBody() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center'
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          backgroundColor: 'grey',
+          width: '95%'
+        }}
+      >
+        Some thing
+      </div>
+    </div>
+  )
+}
