5f2f5ba6f83443ff5760b57432d98e232362bd84
HaoningMou <muhaoning@hotmail.com>
2023-03-05 22:30:22 +1100

    update getData

--- f1-portal/src/api/getData.js
+++ f1-portal/src/api/getData.js
@@ -1,22 +1,22 @@
 import axios from 'axios'
 
 export const getNews = async () => {
-  const result = await axios.get('http://localhost:8081/news/getNews')
+  const result = await axios.get('http://35.90.147.98:8081/news/getNews')
   return result.data
 }
 
 export const getDriver = async () => {
-  const result = await axios.get('http://localhost:8081/driver/getDriver')
+  const result = await axios.get('http://35.90.147.98:8081/driver/getDriver')
   return result.data
 }
 
 export const getDriverById = async (id) => {
-  const result = await axios.get('http://localhost:8081/driver/getDriver/' + id)
+  const result = await axios.get('http://35.90.147.98:8081/driver/getDriver/' + id)
   console.log(result.data[0].id)
   return result.data[0]
 }
 
 export const getSchedule = async () => {
-  const result = await axios.get('http://localhost:8081/schedule/getSchedule')
+  const result = await axios.get('http://35.90.147.98:8081/schedule/getSchedule')
   return result.data
 }
